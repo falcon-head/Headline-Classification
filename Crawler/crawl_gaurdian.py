ef040c08a085fcad9d4604215e00e206292e2563
Shrikrishna Joisa <32938743+falcon-head@users.noreply.github.com>
2020-12-28 12:18:21 +0530

    Updated comments

--- Crawler/crawl_gaurdian.py
+++ Crawler/crawl_gaurdian.py
@@ -25,16 +25,16 @@ def capture_data():
     Capture the data from the gaurdian api
     """
 
-    # List of category data capture from the gaurdian news
-    #category_list = ['technology', 'business', 'culture', 'environment', 'fashion', 'film', 'food', 'lifeandstyle', 'politics', 'science',
-    #'football', 'sport', 'travel', 'music']
+    # List of category data to capture from the gaurdian news
+    category_list = ['technology', 'business', 'culture', 'environment', 'fashion', 'film', 'food', 'lifeandstyle', 'politics', 'science',
+    'football', 'sport', 'travel', 'music']
 
-    category_list = ['technology', 'science', 'fashion', 'culture', 'travel']
+    #category_list = ['technology', 'science', 'fashion', 'culture', 'travel']
 
     # The api url
     api_url = "https://content.guardianapis.com/search?section={section}"
 
-    # The api url
+    # The api date
     api_date="from-date=2018-01-01&to-date=2020-12-26"
 
     # the api page
@@ -50,7 +50,7 @@ def capture_data():
     fields='format=json&show-fields=all&use-date=newspaper-edition'
 
     # api key
-    api_key = config.gaurdian_api_key
+    api_key = config.gaurdian_api_key    # Create a config.py file and create a variable gaurdian_api_key with your API key
 
     # Create the api url and extract the data
     url_link = [api_url, api_date, api_page, api_page_size, fields, api_key]
